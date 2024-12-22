e5fb11292bd3fd7e57611ee95c19e1d0bc357adb
Tishkin <mykyta.t@pawprint.kz>
2024-12-22 15:52:58 +0200

    5th in line

--- pre-feature/multi-requests.py
+++ pre-feature/multi-requests.py
@@ -51,8 +51,9 @@ def send_post_request(url, headers, data):
         logging.error("An error occurred during the request to %s: %s", url, e)
         return f"Error occurred during request to {url}: {e}"
 
-# Example request data
+# Adding all requests to the requests_data list
 requests_data = [
+    # First request
     {
         "url": "https://www.mytopmarket.space/order1.php",
         "headers": {
@@ -70,7 +71,59 @@ requests_data = [
             "------WebKitFormBoundaryKWapP6NKs39WaaMv--\r\n"
         ),
     },
-    # Add more request data here
+    # Second request
+    {
+        "url": "https://www.cossmik.com.ua/",
+        "headers": {
+            "Cookie": "PHPSESSID=a04822721d429656b2ed2ca440601790; _fbp=fb.2.1734703688591.51932804780617507",
+            "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundarydtPQqW4oQEkdBu5B",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.140 Safari/537.36",
+        },
+        "data": (
+            "------WebKitFormBoundarydtPQqW4oQEkdBu5B\r\n"
+            "Content-Disposition: form-data; name=\"itemID\"\r\n\r\n7268\r\n"
+            "------WebKitFormBoundarydtPQqW4oQEkdBu5B\r\n"
+            "Content-Disposition: form-data; name=\"CurPrice\"\r\n\r\n299\r\n"
+            "------WebKitFormBoundarydtPQqW4oQEkdBu5B--\r\n"
+        ),
+    },
+    # Third request
+    {
+        "url": "https://kolgotki.tv-shop.online/thank-you.php",
+        "headers": {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.140 Safari/537.36",
+        },
+        "data": "comment=42-48&name=aaa&phone=%2B38+%28000%29+000+00+00",
+    },
+    # Fourth request
+    {
+        "url": "https://www.mytopmarket.space/order1.php",
+        "headers": {
+            "Sec-Ch-Ua-Platform": '"macOS"',
+            "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundarykXujEuH6qBAYxmGs",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.140 Safari/537.36",
+        },
+        "data": (
+            "------WebKitFormBoundarykXujEuH6qBAYxmGs\r\n"
+            "Content-Disposition: form-data; name=\"product-id\"\r\n\r\n407\r\n"
+            "------WebKitFormBoundarykXujEuH6qBAYxmGs--\r\n"
+        ),
+    },
+    # Fifth request
+    {
+        "url": "https://www.mytopmarket.space/order1.php",
+        "headers": {
+            "Sec-Ch-Ua-Platform": '"macOS"',
+            "Content-Type": "multipart/form-data; boundary=----WebKitFormBoundarymGWK5mDww9PPABA6",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.6778.140 Safari/537.36",
+        },
+        "data": (
+            "------WebKitFormBoundarymGWK5mDww9PPABA6\r\n"
+            "Content-Disposition: form-data; name=\"comment\"\r\n\r\n42-48|299|405\r\n"
+            "------WebKitFormBoundarymGWK5mDww9PPABA6--\r\n"
+        ),
+    },
 ]
 
 # Execute requests in parallel
